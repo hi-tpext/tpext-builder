fc5e41742b4f9d6cebb27f332cfaa811c7bfe6ac
ichynul <ichynul@163.com>
2025-11-21 17:48:27 +0800

    优化弹窗关闭

--- src/common/Module.php
+++ src/common/Module.php
@@ -9,7 +9,7 @@ use tpext\common\Module as baseModule;
 
 class Module extends baseModule
 {
-    protected $version = '1.0.10';
+    protected $version = '1.0.11';
 
     protected $name = 'tpext.builder';
 
